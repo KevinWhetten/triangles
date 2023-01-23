6ed0668308cb0ab875e4ac74b6d107005f11117d
Kevin Whetten <krwhetten@gmail.com>
2023-01-23 16:21:00 -0700

    Removed DrawTriangle for now.

--- triangles/ClientApp/src/app/home/home.component.ts
+++ triangles/ClientApp/src/app/home/home.component.ts
@@ -25,7 +25,6 @@ export class HomeComponent {
       this.angle = '';
       this.sides = '';
     }
-    this.drawTriangle();
   }
 
   setValidity() {
@@ -77,60 +76,6 @@ export class HomeComponent {
       return;
     }
     this.sides = "scalene";
-  }
-
-  drawTriangle() {
-    const canvas = document.getElementById("triangle");
-    // @ts-ignore
-    if (canvas.getContext) {
-      // @ts-ignore
-      const ctx = canvas.getContext("2d");
-
-      let pixelCount = 300;
-
-      if (this.angle == "right") {
-        let longestSide = Math.max(...[this.sideA, this.sideB, this.sideC]);
-
-        if (longestSide == this.sideA) {
-          ctx.moveTo(0, 0);
-          ctx.lineTo(0, pixelCount);
-          let factor = pixelCount / longestSide;
-
-          let longerSide = Math.max(...[this.sideB, this.sideC]);
-
-          if (longerSide == this.sideB) {
-            // (x1+l⋅cos(a),y1+l⋅sin(a))
-            let angle = 90 - this.angleC;
-            let length = this.sideB * factor;
-            let x = length * Math.cos(angle);
-            let y = 300 + length * Math.sin(angle);
-
-            ctx.lineTo(x, y);
-            ctx.lineTo(0, 0);
-            ctx.stroke();
-          } else if (longerSide == this.sideC) {
-
-          }
-        } else if (longestSide == this.sideC) {
-          let longerSide = Math.max(...[this.sideA, this.sideB]);
-          let factor = pixelCount / longerSide;
-
-          if (longerSide == this.sideA) {
-            ctx.moveTo(0, 0);
-            ctx.lineTo(0, pixelCount);
-            ctx.lineTo(factor * this.sideB, pixelCount);
-            ctx.lineTo(0, 0);
-            ctx.stroke();
-          } else if (longerSide == this.sideB) {
-            ctx.moveTo(0, 0);
-            ctx.lineTo(0, factor * this.sideA);
-            ctx.lineTo(pixelCount, factor * this.sideA);
-            ctx.lineTo(0, 0);
-            ctx.stroke();
-          }
-        }
-      }
-    }
   }
 }
 
