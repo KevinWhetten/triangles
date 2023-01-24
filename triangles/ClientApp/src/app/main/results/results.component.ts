690a004e4fd5078ce0d9436a8362167268ed30a2
Kevin Whetten <krwhetten@gmail.com>
2023-01-24 16:40:50 -0700

    Oops. These files were removed in the last commit.

---
+++ triangles/ClientApp/src/app/main/results/results.component.ts
@@ -0,0 +1,9 @@
+﻿import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-results',
+  templateUrl: './results.component.html',
+  styleUrls: ['./results.component.scss']
+})
+export class ResultsComponent {
+}
